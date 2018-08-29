1d2d01e40d7fd1138348fd9457dd4ef0cf685c4e
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-08-29 09:47:42 -0500

    feat: Implement Location

---
+++ app/src/main/java/com/nsutanto/foodfinder/model/Location.java
@@ -0,0 +1,30 @@
+package com.nsutanto.foodfinder.model;
+
+public class Location {
+
+    private String address;
+    private String city;
+    private String latitude;
+    private String longitude;
+    private String zipcode;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+}
