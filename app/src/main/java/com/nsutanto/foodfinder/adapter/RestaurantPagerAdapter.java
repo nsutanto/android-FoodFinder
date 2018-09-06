0c690329609d641e4cf1012b01b843d0ae3725c2
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-05 20:48:44 -0500

    Get the restaurant fragment working

---
+++ app/src/main/java/com/nsutanto/foodfinder/adapter/RestaurantPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.nsutanto.foodfinder.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.nsutanto.foodfinder.R;
+import com.nsutanto.foodfinder.fragment.RestaurantLayoutFragment;
+import com.nsutanto.foodfinder.util.constant;
+
+public class RestaurantPagerAdapter extends FragmentStatePagerAdapter {
+
+    private static final int NUM_PAGES = 3;
+    private Context context;
+
+    public RestaurantPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        RestaurantLayoutFragment restaurantLayoutFragment = new RestaurantLayoutFragment();
+        restaurantLayoutFragment.setFilter(position);
+        return restaurantLayoutFragment;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title;
+        switch (position) {
+            case constant.LUNCH:
+                title = context.getString(R.string.lunch);
+                break;
+            case constant.DINNER:
+                title = context.getString(R.string.dinner);
+                break;
+            case constant.FAVORITE:
+                title =  context.getString(R.string.favorite);
+                break;
+            default:
+                title = "";
+        }
+        return title;
+    }
+
+
+    @Override
+    public int getCount() {
+        return NUM_PAGES;
+    }
+}
