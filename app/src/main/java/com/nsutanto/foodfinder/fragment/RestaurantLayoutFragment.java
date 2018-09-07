1caef49e32580612db60a404274dbb03942337ef
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-06 22:16:31 -0500

    feat: Add Restaurant Layout Fragment

---
+++ app/src/main/java/com/nsutanto/foodfinder/fragment/RestaurantLayoutFragment.java
@@ -0,0 +1,155 @@
+package com.nsutanto.foodfinder.fragment;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.nsutanto.foodfinder.R;
+import com.nsutanto.foodfinder.apiclient.ZomatoClient;
+
+import butterknife.ButterKnife;
+import com.nsutanto.foodfinder.util.constant;
+
+
+public class RestaurantLayoutFragment extends Fragment {
+
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0;
+
+    private int category;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Activity activity;
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_restaurant_layout, container, false);
+        ButterKnife.bind(this, view);
+
+        activity = getActivity();
+        if (savedInstanceState != null) {
+            // TODO:
+        }
+
+        return view;
+    }
+
+
+    private void searchRestaurant() {
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity);
+
+        if (ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(activity,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                double latitude = location.getLatitude();
+                                double longitude = location.getLongitude();
+                                if (category == constant.LUNCH) {
+                                    ZomatoClient.GetRestaurant(latitude, longitude, "lunch");
+                                } else if (category == constant.DINNER) {
+                                    ZomatoClient.GetRestaurant(latitude, longitude, "dinner");
+                                }
+                            } else {
+
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    searchRestaurant();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        searchRestaurant();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
