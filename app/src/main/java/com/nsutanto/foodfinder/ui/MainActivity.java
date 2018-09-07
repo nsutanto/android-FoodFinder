e662d10df3e654748bf99578b69e373bbd2bf917
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-06 22:17:14 -0500

    feat: Setup Pager

--- app/src/main/java/com/nsutanto/foodfinder/ui/MainActivity.java
+++ app/src/main/java/com/nsutanto/foodfinder/ui/MainActivity.java
@@ -1,18 +1,49 @@
 package com.nsutanto.foodfinder.ui;
 
+import android.app.FragmentManager;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.nsutanto.foodfinder.R;
-import com.nsutanto.foodfinder.apiclient.ZomatoClient;
+import com.nsutanto.foodfinder.adapter.RestaurantPagerAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.pager_restaurant)
+    ViewPager vp_restaurant;
+
+    @BindView(R.id.tab_restaurant)
+    TabLayout tab_restaurant;
+
+    private RestaurantPagerAdapter restaurantPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        ZomatoClient.GetRestaurant("Chicago");
+        setupPager();
+    }
+
+    private void setupPager() {
+
+        restaurantPagerAdapter = new RestaurantPagerAdapter(getSupportFragmentManager(), this);
+        vp_restaurant.setAdapter(restaurantPagerAdapter);
+        tab_restaurant.setupWithViewPager(vp_restaurant);
+
+        /*
+        FragmentManager fm = getFragmentManager();
+
+        fm.beginTransaction()
+                .add(R.id.recipe_container, recipeFragment)
+                .commit();
+                */
     }
 }
