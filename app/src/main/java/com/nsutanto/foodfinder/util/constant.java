285beb24a2e07ff7802b6a704f80c80426505147
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-06 22:16:50 -0500

    feat: Add constant for categories

---
+++ app/src/main/java/com/nsutanto/foodfinder/util/constant.java
@@ -0,0 +1,9 @@
+package com.nsutanto.foodfinder.util;
+
+
+
+public final class constant {
+    public static final int LUNCH = 0;
+    public static final int DINNER = 1;
+    public static final int FAVORITE = 2;
+}
