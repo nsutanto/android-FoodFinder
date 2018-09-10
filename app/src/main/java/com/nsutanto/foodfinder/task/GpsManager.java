81d97f78bb24264df2f8d82a4c4b493ec299a5bf
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-09 19:52:32 -0500

    GPS Manager

---
+++ app/src/main/java/com/nsutanto/foodfinder/task/GpsManager.java
@@ -0,0 +1,53 @@
+package com.nsutanto.foodfinder.task;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class GpsManager extends LocationCallback {
+
+    private FusedLocationProviderClient client;
+    private Callback callback;
+
+    public interface Callback {
+        void onLocationResult(LocationResult locationResult);
+    }
+
+    public boolean start(Context context, Callback callback) {
+        this.callback = callback;
+        client = LocationServices.getFusedLocationProviderClient(context);
+        if (!checkLocationPermission(context)) {
+            return false;
+        }
+        client.requestLocationUpdates(getLocationRequest(), this, null);
+        return true;
+    }
+
+    public void stop() {
+        client.removeLocationUpdates(this);
+    }
+
+    @Override
+    public void onLocationResult(LocationResult locationResult) {
+        callback.onLocationResult(locationResult);
+    }
+
+    private boolean checkLocationPermission(Context context) {
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                context, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionCheck == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private LocationRequest getLocationRequest() {
+        return LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(30_000L)
+                .setFastestInterval(20_000L);
+    }
+}
