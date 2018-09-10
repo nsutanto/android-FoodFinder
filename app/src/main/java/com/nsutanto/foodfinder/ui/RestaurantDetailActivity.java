ddf8ef980685a0dedf9ee6a1d403d0678b6b174d
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-10 07:49:08 -0500

    feat: Add restaurant detail activity

---
+++ app/src/main/java/com/nsutanto/foodfinder/ui/RestaurantDetailActivity.java
@@ -0,0 +1,20 @@
+package com.nsutanto.foodfinder.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nsutanto.foodfinder.R;
+
+import butterknife.ButterKnife;
+
+public class RestaurantDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+        ButterKnife.bind(this);
+
+
+    }
+}
