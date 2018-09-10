d4c38a7f76ed43da4bcaf34eaedc1394cbd6ac45
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-10 07:47:10 -0500

    feat: Adding listener

---
+++ app/src/main/java/com/nsutanto/foodfinder/listener/IRestaurantFragmentListener.java
@@ -0,0 +1,7 @@
+package com.nsutanto.foodfinder.listener;
+
+import com.nsutanto.foodfinder.model.Restaurant;
+
+public interface IRestaurantFragmentListener {
+    void onRestaurantClick(Restaurant restaurant);
+}
