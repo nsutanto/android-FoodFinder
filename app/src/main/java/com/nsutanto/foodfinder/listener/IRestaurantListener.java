592e47f53c81bb692e60d7e8f08021d2587d2fe2
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-09 19:51:12 -0500

    feat: Implement restaurant listener

--- app/src/main/java/com/nsutanto/foodfinder/listener/IRestaurantListener.java
+++ app/src/main/java/com/nsutanto/foodfinder/listener/IRestaurantListener.java
@@ -1,4 +1,8 @@
 package com.nsutanto.foodfinder.listener;
 
+import com.nsutanto.foodfinder.model.Restaurant;
+
 public interface IRestaurantListener {
+
+    void onRestaurantClick(Restaurant restaurant);
 }
