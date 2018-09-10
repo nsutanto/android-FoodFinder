c353cc90d0328282e16526d49cf3e91c58e05bf4
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-10 09:06:43 -0500

    feat: Make location parceable

--- app/src/main/java/com/nsutanto/foodfinder/model/Location.java
+++ app/src/main/java/com/nsutanto/foodfinder/model/Location.java
@@ -1,8 +1,11 @@
 package com.nsutanto.foodfinder.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class Location {
+public class Location implements Parcelable {
 
     @SerializedName("address")
     private String address;
@@ -72,5 +75,43 @@ public class Location {
 
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    public static final Parcelable.Creator<Location> CREATOR = new Parcelable.Creator<Location>() {
+
+        @Override
+        public Location createFromParcel(Parcel in) {
+            return new Location(in);
+        }
+
+        @Override
+        public Location[] newArray(int size) {
+            return new Location[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+
+        out.writeString(address);
+        out.writeString(city);
+        out.writeString(city_id);
+        out.writeString(latitude);
+        out.writeString(longitude);
+        out.writeString(zipcode);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    private Location(Parcel in) {
+        address = in.readString();
+        city = in.readString();
+        city_id = in.readString();
+        latitude = in.readString();
+        longitude = in.readString();
+        zipcode = in.readString();
     }
 }
