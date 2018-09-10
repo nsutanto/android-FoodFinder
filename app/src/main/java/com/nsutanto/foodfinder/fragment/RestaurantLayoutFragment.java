dd557617fe77851eb7ac8374de720d591a02c58a
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-09 19:51:00 -0500

    feat: Get the layout fragment working

--- app/src/main/java/com/nsutanto/foodfinder/fragment/RestaurantLayoutFragment.java
+++ app/src/main/java/com/nsutanto/foodfinder/fragment/RestaurantLayoutFragment.java
@@ -1,36 +1,58 @@
 package com.nsutanto.foodfinder.fragment;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.location.LocationResult;
 import com.nsutanto.foodfinder.R;
-import com.nsutanto.foodfinder.apiclient.ZomatoClient;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import com.nsutanto.foodfinder.adapter.RestaurantAdapter;
+import com.nsutanto.foodfinder.listener.IRestaurantListener;
+import com.nsutanto.foodfinder.listener.IRestaurantSearchListener;
+import com.nsutanto.foodfinder.model.Restaurant;
+import com.nsutanto.foodfinder.task.GpsManager;
+import com.nsutanto.foodfinder.task.SearchRestaurantTask;
 import com.nsutanto.foodfinder.util.constant;
 
+import java.util.ArrayList;
 
-public class RestaurantLayoutFragment extends Fragment {
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
-    private final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0;
+
+public class RestaurantLayoutFragment extends Fragment implements GpsManager.Callback, IRestaurantSearchListener, IRestaurantListener {
+
+    @BindView(R.id.rv_restaurant)
+    RecyclerView rv_restaurant;
 
     private int category;
-    private FusedLocationProviderClient fusedLocationClient;
     private Activity activity;
+    private double latitude;
+    private double longitude;
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+    private GpsManager gpsManager;
+    private RestaurantAdapter restaurantAdapter;
+    private GridLayoutManager layoutManager;
+
+
 
     public void setCategory(int category) {
         this.category = category;
@@ -46,91 +68,96 @@ public class RestaurantLayoutFragment extends Fragment {
             // TODO:
         }
 
+        initRecyclerView(view);
         return view;
     }
 
 
-    private void searchRestaurant() {
+    private void startGps() {
+        gpsManager = new GpsManager();
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity);
-
-        if (ContextCompat.checkSelfPermission(activity,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(activity,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // Got last known location. In some rare situations this can be null.
-                            if (location != null) {
-                                double latitude = location.getLatitude();
-                                double longitude = location.getLongitude();
-                                if (category == constant.LUNCH) {
-                                    ZomatoClient.GetRestaurant(latitude, longitude, "lunch");
-                                } else if (category == constant.DINNER) {
-                                    ZomatoClient.GetRestaurant(latitude, longitude, "dinner");
-                                }
-                            } else {
-
-                            }
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            e.printStackTrace();
-                        }
-                    });
+        // check if user gave permissions, otherwise ask via dialog
+        if (!checkPermission()) {
+            getLocationPermissions();
+            return;
         }
 
+        gpsManager.start(activity.getApplicationContext(), this);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    searchRestaurant();
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
+    public void onLocationResult(LocationResult locationResult) {
+        Location location = locationResult.getLastLocation();
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        gpsManager.stop();
+        new SearchRestaurantTask().execute(this);
+    }
 
-            // other 'case' lines to check for other
-            // permissions this app might request.
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public String getCategory() {
+        if (category == constant.LUNCH) {
+            return "lunch";
         }
+        return "dinner";
+    }
+
+    public void onPostExecute(ArrayList<Restaurant> restaurants) {
+        rv_restaurant.setAdapter(restaurantAdapter);
+        restaurantAdapter.setRestaurants(restaurants);
+    }
+
+    public void onRestaurantClick(Restaurant restaurant) {
+
+    }
+
+    private void initRecyclerView(View view) {
+        int numColumn = calculateNoOfColumns(view.getContext());
+        restaurantAdapter = new RestaurantAdapter(this);
+        layoutManager = new GridLayoutManager(view.getContext(), numColumn, GridLayoutManager.VERTICAL, false);
+        rv_restaurant.setLayoutManager(layoutManager);
+        rv_restaurant.setHasFixedSize(true);
+
+        rv_restaurant.setAdapter(restaurantAdapter);
+
+    }
+
+    public static int calculateNoOfColumns(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int scalingFactor = 200;
+        int noOfColumns = (int) (dpWidth / scalingFactor);
+        if(noOfColumns < 2)
+            noOfColumns = 2;
+        return noOfColumns;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void getLocationPermissions() {
+        requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_REQUEST_FINE_LOCATION);
+    }
+
+    private boolean checkPermission() {
+        return isGranted(ActivityCompat.checkSelfPermission(activity, ACCESS_FINE_LOCATION)) &&
+                isGranted(ActivityCompat.checkSelfPermission(activity, ACCESS_COARSE_LOCATION));
+    }
+
+    private boolean isGranted(int permission) {
+        return permission == PackageManager.PERMISSION_GRANTED;
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
-        searchRestaurant();
+        startGps();
     }
 
     @Override
@@ -151,5 +178,6 @@ public class RestaurantLayoutFragment extends Fragment {
     @Override
     public void onStop() {
         super.onStop();
+        gpsManager.stop();
     }
 }
