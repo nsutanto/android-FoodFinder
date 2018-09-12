77279beeecdf9a79b8e760459da4b9f0a28b54d7
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-12 16:26:26 -0500

    change to breakfast

--- app/src/main/java/com/nsutanto/foodfinder/util/constant.java
+++ app/src/main/java/com/nsutanto/foodfinder/util/constant.java
@@ -3,7 +3,7 @@ package com.nsutanto.foodfinder.util;
 
 
 public final class constant {
-    public static final int LUNCH = 0;
+    public static final int BREAKFAST = 0;
     public static final int DINNER = 1;
     public static final int FAVORITE = 2;
 }
