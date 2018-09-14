48c611167d7ee62d43d793724e7cc563f9d0f133
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-13 21:36:26 -0500

    clean up

--- app/src/main/java/com/nsutanto/foodfinder/listener/IRestaurantInfoFragmentListener.java
+++ app/src/main/java/com/nsutanto/foodfinder/listener/IRestaurantInfoFragmentListener.java
@@ -1,6 +1,5 @@
 package com.nsutanto.foodfinder.listener;
 
-import com.nsutanto.foodfinder.model.Restaurant;
 import com.nsutanto.foodfinder.model.RestaurantInfo;
 
 public interface IRestaurantInfoFragmentListener {
