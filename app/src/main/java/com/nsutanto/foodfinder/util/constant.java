b07ac782fd84b84452c252a753d475e96a086529
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-13 21:36:46 -0500

    add constant

--- app/src/main/java/com/nsutanto/foodfinder/util/constant.java
+++ app/src/main/java/com/nsutanto/foodfinder/util/constant.java
@@ -6,4 +6,7 @@ public final class constant {
     public static final int BREAKFAST = 0;
     public static final int DINNER = 1;
     public static final int FAVORITE = 2;
+
+    public static final String ZOMATO_BREAKFAST_ID = "8";
+    public static final String ZOMATO_DINNER_ID = "10";
 }
