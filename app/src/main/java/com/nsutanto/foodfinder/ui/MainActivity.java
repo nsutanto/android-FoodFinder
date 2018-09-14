3d93eca4ea1f56a3977f419e56954c4cfe3f044f
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-13 21:20:12 -0500

    fix favorite

--- app/src/main/java/com/nsutanto/foodfinder/ui/MainActivity.java
+++ app/src/main/java/com/nsutanto/foodfinder/ui/MainActivity.java
@@ -1,6 +1,9 @@
 package com.nsutanto.foodfinder.ui;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,9 +15,14 @@ import com.nsutanto.foodfinder.listener.IRestaurantFragmentListener;
 import com.nsutanto.foodfinder.listener.IRestaurantInfoFragmentListener;
 import com.nsutanto.foodfinder.model.Restaurant;
 import com.nsutanto.foodfinder.model.RestaurantInfo;
+import com.nsutanto.foodfinder.viewmodel.RestaurantViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements IRestaurantFragmentListener, IRestaurantInfoFragmentListener {
 
@@ -25,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements IRestaurantFragme
     TabLayout tab_restaurant;
 
     private RestaurantPagerAdapter restaurantPagerAdapter;
+    private List<RestaurantInfo> favoriteRestaurants = new ArrayList<>();
 
 
     @Override
@@ -33,7 +42,10 @@ public class MainActivity extends AppCompatActivity implements IRestaurantFragme
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        Timber.plant(new Timber.DebugTree());
+
         setupPager();
+        setupFavoriteRestaurantsVM();
     }
 
     public void onRestaurantClick(Restaurant restaurant) {
@@ -46,10 +58,38 @@ public class MainActivity extends AppCompatActivity implements IRestaurantFragme
     }
 
     private void openRestaurantDetail(RestaurantInfo restaurantInfo) {
+        if (isInFavoriteList(restaurantInfo)) {
+            restaurantInfo.setFavorite(1);
+        }
+
         Intent intent = new Intent(this, RestaurantDetailActivity.class);
         intent.putExtra("restaurantInfo", restaurantInfo);
         startActivity(intent);
     }
+
+    private boolean isInFavoriteList(RestaurantInfo restaurantInfo) {
+        boolean isFavorite = false;
+
+        for (RestaurantInfo favoriteRestaurant: favoriteRestaurants) {
+            if (restaurantInfo.getId().equals(favoriteRestaurant.getId())) {
+                isFavorite = true;
+                break;
+            }
+        }
+        return isFavorite;
+    }
+
+    private void setupFavoriteRestaurantsVM() {
+
+        RestaurantViewModel restaurantVM = ViewModelProviders.of(this).get(RestaurantViewModel.class);
+        restaurantVM.getRestaurantInfos().observe(this, new Observer<List<RestaurantInfo>>() {
+            @Override
+            public void onChanged(@Nullable List<RestaurantInfo> restaurantInfos) {
+                favoriteRestaurants = restaurantInfos;
+            }
+        });
+    }
+
 
     private void setupPager() {
 
