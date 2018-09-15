51cc888162474646b9391a71ad90bd7da5de8455
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-09-14 19:32:48 -0500

    fix internet connection

--- app/src/main/java/com/nsutanto/foodfinder/ui/RestaurantLayoutFragment.java
+++ app/src/main/java/com/nsutanto/foodfinder/ui/RestaurantLayoutFragment.java
@@ -42,7 +42,7 @@ import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 
-public class RestaurantLayoutFragment extends Fragment implements GpsManager.Callback, IRestaurantSearchListener, IRestaurantAdapterListener {
+public class RestaurantLayoutFragment extends Fragment implements IRestaurantSearchListener, IRestaurantAdapterListener {
 
     @BindView(R.id.rv_restaurant)
     RecyclerView rv_restaurant;
@@ -51,8 +51,6 @@ public class RestaurantLayoutFragment extends Fragment implements GpsManager.Cal
     private Activity activity;
     private double latitude;
     private double longitude;
-    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
-    private GpsManager gpsManager;
     private RestaurantAdapter restaurantAdapter;
     private GridLayoutManager layoutManager;
     private IRestaurantFragmentListener iRestaurantFragmentListener;
@@ -66,13 +64,13 @@ public class RestaurantLayoutFragment extends Fragment implements GpsManager.Cal
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_restaurant_layout, container, false);
         ButterKnife.bind(this, view);
-
         activity = getActivity();
         if (savedInstanceState != null) {
             // TODO:
         }
 
         initRecyclerView(view);
+        fetchSearchRestaurant();
         return view;
     }
 
@@ -88,25 +86,7 @@ public class RestaurantLayoutFragment extends Fragment implements GpsManager.Cal
         }
     }
 
-
-    private void startGps() {
-        gpsManager = new GpsManager();
-
-        // check if user gave permissions, otherwise ask via dialog
-        if (!checkPermission()) {
-            getLocationPermissions();
-            return;
-        }
-
-        gpsManager.start(activity.getApplicationContext(), this);
-    }
-
-    @Override
-    public void onLocationResult(LocationResult locationResult) {
-        Location location = locationResult.getLastLocation();
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
-        gpsManager.stop();
+    public void fetchSearchRestaurant() {
 
         ConnectivityManager cm = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo info = cm.getActiveNetworkInfo();
@@ -135,9 +115,13 @@ public class RestaurantLayoutFragment extends Fragment implements GpsManager.Cal
         return latitude;
     }
 
+    public void setLatitude(Double latitude) { this.latitude = latitude; }
+
     public double getLongitude() {
         return longitude;
     }
+
+    public void setLongitude(Double longitude) { this.longitude = longitude; }
 
     public String getCategory() {
         if (category == constant.BREAKFAST) {
@@ -179,26 +163,7 @@ public class RestaurantLayoutFragment extends Fragment implements GpsManager.Cal
         return noOfColumns;
     }
 
-    @TargetApi(Build.VERSION_CODES.M)
-    private void getLocationPermissions() {
-        requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
-                PERMISSION_REQUEST_FINE_LOCATION);
-    }
 
-    private boolean checkPermission() {
-        return isGranted(ActivityCompat.checkSelfPermission(activity, ACCESS_FINE_LOCATION)) &&
-                isGranted(ActivityCompat.checkSelfPermission(activity, ACCESS_COARSE_LOCATION));
-    }
-
-    private boolean isGranted(int permission) {
-        return permission == PackageManager.PERMISSION_GRANTED;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        startGps();
-    }
 
     @Override
     public void onResume() {
@@ -218,6 +183,5 @@ public class RestaurantLayoutFragment extends Fragment implements GpsManager.Cal
     @Override
     public void onStop() {
         super.onStop();
-        gpsManager.stop();
     }
 }
